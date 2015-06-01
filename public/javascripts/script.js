cefd335db0689be706a8fbeba0b97fbc1c7193ea
Marco Sportillo <msportillo@gmail.com>
2015-06-02 01:17:48 +0200

    new messages

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,7 +3,12 @@ var messages =
 	'1. Che tu possa avere sempre il vento in poppa, che il sole ti risplenda in viso e che il vento del destino ti porti in alto a danzare con le stelle.',
 	'2. Auguri!',
 	'3. Auguri #nomedelfesteggiato',
-	'4. Happy Birthday'
+	'4. Happy Birthday',
+	'5. Tanti auguri',
+	'6. Un milione di questi giorni!',
+	'7. Milioni di auguri!',
+	'8. Che tu possa avere sempre il vento in poppa, che il sole ti risplenda in viso e che il vento del destino ti porti in alto a danzare con le stelle.',
+	'9. Tanti tanti auguri!'
 ];
 
 function getRandomGreetingMessage() {
